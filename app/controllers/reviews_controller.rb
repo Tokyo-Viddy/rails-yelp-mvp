e8336ae155d5bad55f9ca73f44eaa2ff14a79593
David Gregorato <hydrosnake@gmail.com>
2021-08-11 16:57:57 +0900

    Started linking restaurant model and review model, still unfinished.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,13 @@
-class ReviewController < ApplicationController
+class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to review_path(@review)
     else
